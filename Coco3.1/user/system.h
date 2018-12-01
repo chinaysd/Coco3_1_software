675dbd531a4cee1836a56b2d731c11f323e17ceb
ysd <ysd@emomo.com>
2018-12-01 16:39:47 +0800

    [master]first commit

---
+++ Coco3.1/user/system.h
@@ -0,0 +1,24 @@
+#ifndef _SYSTEM_H_
+#define _SYSTEM_H_
+
+#include "sc92f837x_c.h"
+#include "TimeOut.h"
+#include "bsp_led.h"
+#include "bsp_timer.h"
+#include "bsp_uart.h"
+#include "key.h"
+#include "sys_config.h"
+
+#define WATCH_DOG_CLEAR
+#define WATCH_DOG_ENABLE
+
+
+
+
+void System_Init(void);
+void System_Handle(void);
+
+
+
+
+#endif
